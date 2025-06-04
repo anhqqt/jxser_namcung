08fbcf6c635fe409cc9a8a02c873476e2270d0cd
Anh Quach <tuananh.qaa@gmail.com>
2025-06-04 21:34:40 +0700

    chinh newworld script them ham bi thieu

--- server1/script/maps/newworldscript_h.lua
+++ server1/script/maps/newworldscript_h.lua
@@ -199,6 +199,17 @@ function NewPractice_B(bIn)
 	end
 end
 
+local TONGCLAIMWAR = function (bIn)
+	if (bIn == 1) then
+		SetProtectTime(18*3)
+		AddSkillState(963,1, 0, 18*3)
+	end
+end
+
+local NOTONGCLAIMWAR = function (bIn)
+
+end
+
 -- 进入/离开可以修炼精力的地图
 --function JingliEntrance(bIn)
 --	DynamicExecute("\\script\\global\\jingli.lua", "WorldEntrance", PlayerIndex, bIn)
@@ -207,13 +218,13 @@ end
 aryFuncStore["PARTNER_OFF"] = SetPartnerSwitch
 aryFuncStore["CD_Forbid_OFF"] = CD_ForbidEnemy
 aryFuncStore["CreateTeam_OFF"] = forbidCreateTeam
--- aryFuncStore["USETOWNP_OFF"] = UseTownPSwitch	-- 功能同 forbidUseTownP
+-- aryFuncStore["USETOWNP_OFF"] = UseTownPSwitch	-- ### forbidUseTownP
 aryFuncStore["LOGINREVOUT_OFF"] = setLogoutRevOut
 aryFuncStore["PUNISH_OFF"] = noPunish
 aryFuncStore["TISHENZHIREN"] = setTiShenZhiFu
 aryFuncStore["USETOWNP_OFF"] = forbidUseTownP
 aryFuncStore["FIGHTSTATE_OFF"] = setPeaceState
-
+aryFuncStore["FIGHTSTATE_ON"] = _ForceFightState
 aryFuncStore["SJFIGHTSTATE_OFF"] = sjFightState
 aryFuncStore["HEART_OFF"] = forbidHeart
 aryFuncStore["TONG_MAP"] = TongMap
@@ -223,4 +234,5 @@ aryFuncStore["PUNISH_PK10"] = PK10_InPrison
 aryFuncStore["NATIONALWAR"] = nationalwar_mapevent
 aryFuncStore["NEWPRACTICE_A"] = NewPractice_A
 aryFuncStore["NEWPRACTICE_B"] = NewPractice_B
---aryFuncStore["JINGLI"] = JingliEntrance
+aryFuncStore["TONGCLAIMWAR"] = TONGCLAIMWAR
+aryFuncStore["NOTONGCLAIMWAR"] = NOTONGCLAIMWAR
