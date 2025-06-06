8ef83cd2f9c1fd16a66c60fa98fce69b7ad20748
Anh Quach <sheid1309@gmail.com>
2025-06-07 03:20:30 +0700

    chuyen script npc VA ve thu muc chung + chinh pt VA = 1

---
+++ server1/script/global/anhquach/env.lua
@@ -0,0 +1,10 @@
+---------------------------------------------------------------------------------
+-- File name    : env.lua
+-- Author       : Anh Quach
+-- Date         : 07-06-2025
+-- Description  : Quy ®Þnh c¸c biÕn dïng chung cho c¶ server
+---------------------------------------------------------------------------------
+
+-- V­ît ¶i
+VUOT_AI_THANH_VIEN = 1
+VUOT_AI_LV_TOI_THIEU = 50
