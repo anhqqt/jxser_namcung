e7be12185de233c4dd03903458f6ebd043ded93b
Anh Quach <sheid1309@gmail.com>
2025-06-08 03:09:55 +0700

    quan ly gia ngua Tham Cau va cap do toi thieu Da Tau bang env.lua

--- server1/script/global/anhquach/env.lua
+++ server1/script/global/anhquach/env.lua
@@ -6,6 +6,16 @@
 ---------------------------------------------------------------------------------
 
 -- V­ît ¶i
-VUOT_AI_THANH_VIEN      = 1
-VUOT_AI_LV_TOI_THIEU    = 50
-VUOT_AI_TIEN            = 10000 -- 1 v¹n
+VUOT_AI_THANH_VIEN  = 1
+VUOT_AI_LV_MIN      = 50
+VUOT_AI_TIEN        = 10000 -- 1 v¹n
+
+-- D· TÈu
+DA_TAU_LV_MIN = 30
+
+-- Th©m C©u
+THAM_CAU_O_VAN      = 1500
+THAM_CAU_XICH_THO   = 1800
+THAM_CAU_TUYET_ANH  = 1500
+THAM_CAU_DICH_LO    = 1500
+THAM_CAU_CHIEU_DA   = 2000
